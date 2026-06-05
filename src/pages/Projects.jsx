2e10e639c0c63ff71f58a7fbf2cff6c8ae3d4c2c
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-06-05 23:24:30 +0530

    added more projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,29 +6,56 @@ function Projects() {
 
   const projects = [
 
-    
-    {
-      title: "h4chat",
-      live: "https://h4chat.vercel.app/",
-      repo: "https://https://github.com/h4dotai/h4chat",
-      desc: "Random terminal style encrypted chat app...just realtime private communication with Go + WebSockets."
-    },
-    
-    {
-      title: "chessBee",
-      live: "https://chessbee.vercel.app/",
-      repo: "https://github.com/harshbarnawa/chessbee",
-      desc: "Online/Offline chess platform with authentication, realtime gameplay logic and custom UI systems."
-    },
-    
-    {
-      title: "editostudios",
-      live: "https://edito-studios.vercel.app/",
-      repo: "https://github.com/harshbarnawa/editoStudios",
-      desc: "Minimal creative agency website built with React and modern frontend interactions."
-    }
+  {
+    title: "OpenGuido",
+    live: "https://marketplace.visualstudio.com/items?itemName=harshbarnawa.openguido",
+    repo: "https://github.com/harshbarnawa/OpenGuidos",
+    desc: "VS Code extension for easy code snippets, custom commands, competitive programming toolkit and faster development workflow."
+  },
 
-  ]
+  {
+    title: "h4chat",
+    live: "https://h4chat.vercel.app/",
+    repo: "https://github.com/h4dotai/h4chat",
+    desc: "Random terminal style encrypted chat app...just realtime private communication with Go + WebSockets."
+  },
+
+  {
+    title: "chessBee",
+    live: "https://chessbee.vercel.app/",
+    repo: "https://github.com/harshbarnawa/chessbee",
+    desc: "Online/Offline chess platform with authentication, realtime gameplay logic and custom UI systems."
+  },
+  {
+    title: "Go-Learn",
+    live: "https://github.com/harshbarnawa/Go-learn",
+    repo: "https://github.com/harshbarnawa/Go-learn",
+    desc: "Backend development journey with Go, APIs, databases, concurrency and modern server-side concepts."
+  },
+
+
+  {
+    title: "editostudios",
+    live: "https://edito-studios.vercel.app/",
+    repo: "https://github.com/harshbarnawa/editoStudios",
+    desc: "Minimal creative agency website built with React and modern frontend interactions."
+  },
+
+  {
+    title: "CodeForces",
+    live: "https://codeforces.com/profile/harshbarnawa.info",
+    repo: "https://github.com/harshbarnawa/CodeForces",
+    desc: "Competitive programming, algorithmic problem solving, contest participation and advanced C++ practice."
+  },
+
+  {
+    title: "LeetCode",
+    live: "https://leetcode.com/u/harshtemp/",
+    repo: "https://github.com/harshbarnawa/leetCode",
+    desc: "Data Structures & Algorithms, interview preparation and problem solving across multiple topics."
+  }
+
+];
 
   return (
 
